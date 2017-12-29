f1e8ad5628d802bb0b08aac06a2358ef9c4093f4
chandanch <chandan.cec107@gmail.com>
2017-12-29 13:33:32 +0530

    refactor(rootModule): cleanup unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { LoginComponent } from './../components/login/login';
 import { Labels } from './../utilities/labels';
 import { LoginModal } from './../pages/login-modal/login-modal';
 import { SignupModalPage } from '../pages/signup-modal/signup-modal';
