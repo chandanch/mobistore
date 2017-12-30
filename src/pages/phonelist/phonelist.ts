4f0d7bf6d89d419c1f89cfe3741361672c33661b
chandanch <chandan.cec107@gmail.com>
2017-12-30 11:41:15 +0530

    feat(signout):  signout with firebase

--- src/pages/phonelist/phonelist.ts
+++ src/pages/phonelist/phonelist.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as firebase from "firebase";
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
 
 import { HttpService } from '../../providers/http-service/http-service';
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
@@ -19,26 +21,77 @@ export class PhonelistPage {
     private http: HttpClient,
     private httpService: HttpService,
     private alertController: AlertController,
+    private loadingController: LoadingController,
     public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PhonelistPage');
     this.loadData();
+    
+  }
+
+  loadData() {
+    // subscribe to the http event stream
     this.httpService.get('assets/phones.json').subscribe(
       data => {
-        console.log('From service', data); 
+        this.phoneList = data;
       }
     )
   }
 
-  loadData() {
-    this.http.get('assets/phones.json').subscribe(
-      data => {
-        console.log('data', data);
-        this.phoneList = data;
-      }
-    )
+  signout() {
+    // first show prompt to the user for signout
+    let signoutAlert = this.alertController.create({
+      title: 'User Signout',
+      message: 'Are you sure you want to signout?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('signout');
+            this.firebaseSignout();
+          }
+        },
+        {
+          text: 'No',
+          role: 'cancel'
+
+        }
+      ]
+    })
+    signoutAlert.present();
+  }
+
+  /**
+   * @desc signout the user from the firebase
+   */
+  firebaseSignout() {
+    let loading = this.loadingController.create({
+      spinner: 'bubbles',
+      content: 'Signing out...'
+    })
+    // call firebase signout once the loading is presented
+    loading.present().then(() => {
+      firebase.auth().signOut().then(
+        success => {
+          // on success dismiss the loading
+          console.log('Signed out');
+          // show login screen once after successfull logout & loading dismiss
+          loading.dismiss().then(() => {
+            this.homepage();
+          });
+        },
+        error => {
+          console.log('Error while signing out');
+          loading.dismiss();
+        }
+      )
+    })
+  }
+
+  homepage() {
+    this.navCtrl.pop();
   }
 
 }
